a9654e1ad36709ab0314e278422d46733092e0f2
samuel7ven <psalmuelsamm@gmail.com>
2021-06-17 09:46:29 -0700

    modified 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main: Prints string
- * Description: Using Printf
+ * main - Prints string
+ * Description: Using printf
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
