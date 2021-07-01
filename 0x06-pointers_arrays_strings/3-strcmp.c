3b18c3ad2fc395cf3a7c6eb64a7ade9b927da869
samuel7ven <psalmuelsamm@gmail.com>
2021-07-01 10:08:41 -0700

    modify 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,22 +3,22 @@
 /**
  * _strcmp - compares two strings
  * @s1: string 1
- * @a2: string 2
- * Return: 1 or 0
- */.
+ * @s2: string 2
+ * Return: 1 if true, 0 if false
+ */
 int _strcmp(char *s1, char *s2)
 {
-	int isEqual = 0;
+	int equal = 0;
 
 	while (*s1)
 	{
 		if (*s1 != *s2)
 		{
-			isEqual = ((int)*s1 - 48) - ((int)*s2 - 48);
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
 			break;
 		}
 		s1++;
 		s2++;
 	}
-	return (isEqual);
+	return (equal);
 }
